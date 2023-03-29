a75e8bf3ee1e552d0cee91a2b365d54761b35d8c
Sasha.Getun <sashagetun@gmail.com>
2023-03-29 23:55:04 +0200

    add Homework_4

--- Homeworks/Homework_4/scripts/script.js
+++ Homeworks/Homework_4/scripts/script.js
@@ -5,12 +5,11 @@ const strings1 = ['hello', 'apple', 'orange', 'good morning', 'danke', 'duck', '
 
 function uppFirstLetter(arrStr) {
   return arrStr.map(str => str.replace(/^\w/, c => c.toUpperCase()));
-}        
-  
+}
 const uppLetter = uppFirstLetter(strings1)
 
 console.log(uppLetter)
-
+//===========================================================================
 const firstLetterUp = arr => arr.map(str => str.charAt(0).toUpperCase() + str.slice(1));
 const firstLetterUp1 = arr => arr.map(str => str[0].toUpperCase() + str.slice(1));
 
@@ -18,7 +17,7 @@ console.log(firstLetterUp1(['hello', 'hey', 'apple', 'orange']));
 
 // 2. Напишите функцию, которая принимает массив чисел и возвращает новый массив чисел, содержащий только положительные числа.
 
-const arr = [7, 9, -14, 3, -8, -3, 77, 9]
+const arr = [7, 9, -14, 3, -8, -3, 77, 9, 12, 17]
 let positiveNumbers = [];
 function getPositiveNumbers(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -30,12 +29,10 @@ function getPositiveNumbers(arr) {
 }
 console.log(getPositiveNumbers(arr));
 
-// const positiveNumbers2 =[];
-function getPositiveNumbers2(arr) {
-  return arr.filter(arr => arr > 0);
-}
-const positivResult = getPositiveNumbers2(arr);
-console.log(positivResult);
+//===========================================
+const getPositiveNumbers2 = getArr => getArr.filter(el => el > 0);
+
+console.log(getPositiveNumbers2(arr));
 
 // 3. Напишите функцию, которая принимает массив чисел и возвращает новый массив чисел, содержащий только нечетные числа.
 
@@ -50,6 +47,11 @@ function getOddNumbers(arr) {
 }
 console.log(getOddNumbers(arr));
 
+//=======================================
+const getOddNumbers_filter = num => num.filter(el => el % 2 !== 0);
+
+console.log(getOddNumbers_filter(arr));
+
 // 4. Создайте новый массив, содержащий только элементы исходного массива, которые делятся на три без остатка.
 
 let threeNumbers = [];
@@ -63,12 +65,17 @@ function getThreeNumbers(arr) {
 }
 console.log(getThreeNumbers(arr));
 
+//=======================================
+const getThreeNumbers_filter = getNum => getNum.filter(el => el % 3 === 0);
+
+console.log(getThreeNumbers_filter(arr));
+
 // 5. Создайте новый массив, содержащий только те элементы исходного массива, которые больше чем 10 и меньше чем 20.
 
 let tenTwentyNumbers = [];
 function getTenTwentyNumbers(arr) {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 10 || arr[i] < 20) {
+    if (arr[i] > 10 && arr[i] < 20) {
       tenTwentyNumbers.push(arr[i]);
     }    
   }
@@ -76,10 +83,15 @@ function getTenTwentyNumbers(arr) {
 }
 console.log(getTenTwentyNumbers(arr));
 
+//==========================================
+const tenTwentyNumbers_filter = arr.filter(el => el > 10 && el < 20);
+
+console.log(tenTwentyNumbers_filter);
+
 // 6. Создайте новый массив, содержащий только те элементы исходного массива, которые не являются строками.
 const numbersString = [7, 78, 'first', 37, -77, -88, 99,'seven', 12, 18];
 
-const newNotString = numbersString.filter(numbersString => typeof numbersString !== 'string');
+const newNotString = numbersString.filter(el => typeof el !== 'string');
 console.log(newNotString)
 
 // *7. Макет: https://www.figma.com/file/RbKedig5BcfNRyZ64Tdfow/Lamp?t=WJWTgZs8pWAWRVRE-0
