a4a6a2acc4a22988b69ea1b4e95eb1db6700ddc3
Sasha.Getun <sashagetun@gmail.com>
2023-03-30 21:45:03 +0200

    add Homework_5

--- Homeworks/Homework_5/scripts/script.js
+++ Homeworks/Homework_5/scripts/script.js
@@ -33,11 +33,11 @@ const users = [
 
 // ДЗ (массив users)
 console.log("1. Сформировать новый массив, в который попадут только возраст пользователей => [28, 18, 32, 14, 54]");
+console.log(users.map(({ age }) => age));
+
 const agesUsers = users.map(el => el.age).filter(el => Number.isInteger(el));  
 
 console.log(agesUsers); 
-
-// console.log(users.map(({ age }) => age));
 
 console.log("2. Сформировать новый массив, в который попадет информация о каждом пользователе в формате 'Name: <firstname>, Age: <age>'");
 users.forEach(el => {
@@ -54,21 +54,31 @@ console.log(usersI);
 
 // ***
 console.log("1. Сформировать новый массив, в который попадет информация о каждом совершеннолетнем пользователе в формате 'Name: <firstname>, Age: <age>'");
-users.forEach(el => {
-  const { firstname, age } = el;
-  if (age > 18) {
-  console.log(`Name: ${firstname}, Age: ${age}`)
-  }
-});
+// users.forEach(el => {
+//   const { firstname, age } = el;
+//   if (age > 18) {
+//   console.log(`Name: ${firstname}, Age: ${age}`)
+//   }
+// });
+
+users.forEach(({ firstname, age }) => age > 18 ? console.log(`Name: ${firstname}, Age: ${age}`) : '');
 
 console.log("2. Сформировать новый массив, в который попадут только активные и совершеннолетние пользователи");
-const activeUsers = users.filter(el => users.age > 18 && users.active === true);
-
-console.log(activeUsers);
+console.log(users.filter(el => el.age >= 18 && el.active === true));
 
 
 // 3. Сформировать новый массив без пользователей, чье имя начинается на букву O
+
+const userArr = [];
+users.map(el => el.firstname[0].toLowerCase() !== 'o' ? userArr.push(el) : '')
+console.log(userArr);
+
 // 4. Сформировать новый массив, в который попадут только имена активных и совершеннолетних пользователей в нижнем регистре
+const userArr2 = [];
+users.forEach(({ firstname, age, active }) => age >= 18 && active === true ? userArr2.push(firstname) : '');
+
+console.log(userArr2);
+
 // ****
 // 1. Сформировать новый массив, где пользователи будут отсортированы по возрасту (.sort())
 // 2. Найти сумму возрастов всех пользователей (.reduce())
