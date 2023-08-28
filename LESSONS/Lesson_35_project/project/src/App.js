fb380c4548a7386c4eea2cd537be0e15dc2c42ca
Sasha.Getun <sashagetun@gmail.com>
2023-08-28 15:59:30 +0200

    pre project and project

--- LESSONS/Lesson_35_project/project/src/App.js
+++ LESSONS/Lesson_35_project/project/src/App.js
@@ -2,12 +2,29 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Footer';
 import NavMenu from './NavMenu';
-
+import MainPage from './pages/MainPage';
+import AllCategoriesPage from './pages/AllCategoriesPage';
+import AllProductsPage from './pages/AllProductsPage';
+import ProductsByCategoryPage from './pages/ProductsByCategoryPage';
+import SingleProductPage from './pages/SingleProductPage';
+import CartPage from './pages/CartPage';
+import NotFoundPage from './pages/NotFoundPage';
+ 
 
 function App() {
   return (
     <div>
-      App
+      <NavMenu />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/categories' element={<AllCategoriesPage />} />
+        <Route path='/categories/:id' element={<ProductsByCategoryPage />} />
+        <Route path='/products' element={<AllProductsPage />} />
+        <Route path='/products/:id' element={<SingleProductPage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>      
+      <Footer />
     </div>
   );
 }
--- LESSONS/Lesson_35_project/project/src/NavMenu/index.jsx
+++ LESSONS/Lesson_35_project/project/src/NavMenu/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function NavMenu() {
   return (
-    <div>NavMenu</div>
+    <div>
+      <Link to='/'>Main</Link>
+      <Link to='/categories'>All categories</Link>
+      <Link to='/products'>All Products</Link>
+      <Link to='/cart'>Cart</Link>
+    </div>
   )
 }
